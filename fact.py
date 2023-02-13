41011134e399233b165d0b85bb7a5a4d0a8bda2f
reakwon lucciano <g00382913@gmit.ie>
2023-02-13 15:39:14 +0000

    definition

---
+++ fact.py
@@ -0,0 +1,1 @@
+Graph isomorphism is an equivalence relation on graphs and as such it partitions the class of all graphs into equivalence classes. A set of graphs isomorphic to each other is called an isomorphism class of graphs
