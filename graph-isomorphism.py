0ce776d69c178b6f9df4ad7c6df2219f8daaa28f
reakwonlucciano <g00382913@gmit.ie>
2023-02-15 18:11:16 +0000

    hello

--- graph-isomorphism.py
+++ graph-isomorphism.py
@@ -1,1 +1,3 @@
 hello 
+
+Graph isomorphism is an equivalence relation on graphs and as such it partitions the class of all graphs into equivalence classes. A set of graphs isomorphic to each other is called an isomorphism class of graphs.
